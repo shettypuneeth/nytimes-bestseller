422c58ed7e31dfcb001e8efa7b26fa225e83ddfc
Puneeth Shetty <puneeth102@gmail.com>
2018-02-24 17:30:27 +0530

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # NY TIMES BESTSELLER 
+
+## React Native and mobx-state-tree powered app to browse through the NY time best seller list
--- app/config/environment.js
+++ app/config/environment.js
@@ -3,9 +3,9 @@
 // Place your API_KEY here
 // You can get it from: 
 //  https://docs.google.com/forms/d/e/1FAIpQLSfkmEBqH76HLMMiCC-GPPnhcvHC9aJS86E32dOd0Z8MpY2rvQ/viewform
-export const GOOGLE_BOOKS_API_KEY = 'AIzaSyBmaRRjVx7zMXBJY4UrDVmXxhwdGj1Dbso';
+export const GOOGLE_BOOKS_API_KEY = '';
 
-export const NY_TIMES_API_KEY = '0b7e74336ce645b8a4d821e0b0cabb60';
+export const NY_TIMES_API_KEY = '';
 
 export const GOOGLE_BOOKS_BASE_URL = 'https://www.googleapis.com/books/v1/volumes';
 
